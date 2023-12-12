1a887085468299b92d2feedacc5116c474dba738
glaucoSapucaia <glaucopco@gmail.com>
2023-12-11 21:35:31 -0300

    Tests whit scripts and commands .sh (shell)

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
-  sleep 1
-done
-
-echo "Postgres Database Started Successfully ($POSTGRES_HOST $POSTGRES_PORT)"
-
-python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:8000
+wait_postgresql.sh
+collectstatic.sh
+makemigrations.sh
+migrate.sh
+runserver.sh
