c75578a588c54c75fe51f0005d90cd99073c4a6a
glaucoSapucaia <glaucopco@gmail.com>
2023-12-12 16:06:17 -0300

    Create superuser and app blog | Config blog views, urls, static paths and templates paths (with base, partials and pages htmls

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # blog views
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
 
