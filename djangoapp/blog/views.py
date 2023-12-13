891d825a60b93104e87adaf8b2061318d76279b3
glaucoSapucaia <glaucopco@gmail.com>
2023-12-13 15:30:30 -0300

    Big CSS and html update | Code from https://github.com/luizomf

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
+posts = list(range(1000))
+
 def index(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+    }
     return render(
         request,
-        'blog/pages/index.html'
+        'blog/pages/index.html',
+        context
+    )
+
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html'
+    )
+
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html'
     )
