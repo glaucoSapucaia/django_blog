ee1c841b460630becf9deb3c60a918e32273c329
glaucoSapucaia <glaucopco@gmail.com>
2023-12-15 17:31:26 -0300

    Create created_by and category views and urls | Update post.html with user informations | Tests with django shell to make to duplicate posts

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -20,13 +20,50 @@ def index(request):
         context
     )
 
-def post(request):
+def created_by(request, author_pk):
+    posts = Post.my_objects.isPublished().filter(created_by__pk=author_pk)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+    }
     return render(
         request,
-        'blog/pages/post.html'
+        'blog/pages/index.html',
+        context
     )
 
-def page(request):
+def category(request, slug):
+    posts = Post.my_objects.isPublished().filter(category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(
+        request,
+        'blog/pages/index.html',
+        context
+    )
+
+def post(request, slug):
+    post = Post.my_objects.filter(slug=slug).first()
+    context = {
+        'post': post,
+    }
+    return render(
+        request,
+        'blog/pages/post.html',
+        context,
+    )
+
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
