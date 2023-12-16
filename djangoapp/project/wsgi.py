8d61c3bb71f41031d733721779e2f15a4263d4ee
glaucoSapucaia <glaucopco@gmail.com>
2023-12-16 13:57:54 -0300

    Config django AXES and python-dotenv in project.settings and project.wsgi | Update requirements.txt

--- djangoapp/project/wsgi.py
+++ djangoapp/project/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+from pathlib import Path
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
+# dotenv config
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_DIR = BASE_DIR.parent / 'dotenv_files' / '.env'
+
+load_dotenv(ENV_DIR, override=True)
+
+# django config
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
